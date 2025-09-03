c26ddf910a20058d13a1e001bcbc8b49ea9f3832
alex <alex@kali>
2025-09-02 20:20:39 -0400

    Minor cha00nges

--- billing/urls.py
+++ billing/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('webhook/stripe/', views.stripe_webhook, name='stripe_webhook'),
     path('transactions/', views.transactions, name='transactions'),
     path('admin/', views.admin_dashboard, name='admin_dashboard'),
+    path('admin/settings/', views.settings, name='settings'),
+    path('api/dashboard-data/', views.dashboard_data, name='dashboard_data'),
 ]
